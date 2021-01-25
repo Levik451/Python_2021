2a33d6f7db74b0a93cdebe5e5261f452cda87d18
igor <yalevik@yandex.ru>
2021-01-25 16:44:53 +0300

    add new file

---
+++ homework_02.py
@@ -0,0 +1,12 @@
+user_time = int(input('Введите время в секундах: '))
+
+hour_convert = user_time // 3600
+minutes_convert = (user_time % 3600) // 60
+sec_convert = (user_time % 3600) % 60
+
+print(f"Время равно: {hour_convert}:{minutes_convert}:{sec_convert}")
+
+# #Отладка
+# print(hour_convert)
+# print(minutes_convert)
+# print(sec_convert)
