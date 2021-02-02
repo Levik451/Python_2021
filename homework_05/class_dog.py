6fe48485bfc671a20c211c8b224041d0279bd3b3
igor <yalevik@yandex.ru>
2021-02-02 17:59:15 +0300

    add class dog

---
+++ homework_05/class_dog.py
@@ -0,0 +1,26 @@
+
+# Создаем простой класс собака
+class Dog:
+
+    # Задаем специальный метод, который автоматически выполняется при создании каждого нового экземпляра
+    def __init__(self, name, age):
+        # Инициализируем атрибуты name и age
+        self.name = name
+        self.age = age
+
+    def sit(self):
+    # Собака садится по команде
+        print(self.name.title() + " is now sitting.")
+
+    def roll_over(self):
+    # Собака перекатывается по команде
+        print(self.name.title() + " rolled over!")
+
+
+#my_dog = Dog('willy', 6)
+dog_input_name = input("Введите кличку собаки: ").lower()
+dog_input_age = input("Введите возраст собаки: ")
+my_dog = Dog(dog_input_name, dog_input_age)
+
+print(f"My dog's name is {my_dog.name.title()}.")
+print(f"My dog is {my_dog.age} years old")
